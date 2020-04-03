7602222518d45398ae6c0a777b18edd4557e683b
rajagond12 <58048685+rajagond12@users.noreply.github.com>
2020-04-03 18:16:01 +0530

    Update carsell.cpp

--- carsell.cpp
+++ carsell.cpp
@@ -10,13 +10,14 @@ void solve(void){
     const unsigned int M = 1000000007;
     unsigned long long int n;
     cin>>n;
-    unsigned long long int a[n];
+    int a[n];
     for(int i=0;i<n;i++) cin>>a[i];
     sort(a,a+n);
     //for(int i=0;i<n;i++) cout<<a[i]<<endl;
-    unsigned long long int sum=0,f=0;
+    unsigned long long int sum=0;
+    int f=0;
     for(int i=n-1;i>=0;i--){
-        if(a[i]<=0) break;
+        if((a[i]-f)<=0) break;
         else {
             a[i]=(a[i]-f)%M;
             sum=((sum%M)+(a[i]%M))%M;
