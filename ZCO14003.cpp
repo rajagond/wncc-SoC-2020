a4c387b034bd42344679fcaf529ff3293a412523
rajagond12 <58048685+rajagond12@users.noreply.github.com>
2020-04-07 15:21:11 +0530

    Create ZCO14003.cpp

---
+++ ZCO14003.cpp
@@ -0,0 +1,35 @@
+.* Problem link
+https://www.codechef.com/LRNDSA01/problems/ZCO14003
+*/
+#include <algorithm>
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+void solve(){
+   long long int n;
+   cin>>n;
+   long long int a[n];
+   for(int i=0;i<n;i++) cin>>a[i];
+   sort(a,a+n);
+   long long int max_pro=0;
+    for(int i=0;i<n;i++){
+        long long int temp=a[i]*(n-i);
+        if(temp>max_pro) max_pro=temp;
+    }
+   cout<<max_pro<<'\n';
+   return ;
+}
+int main()
+{
+//    freopen("input.txt", "r", stdin);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t=1;
+    //cin >> t;
+    while(t --> 0)
+        solve();
+
+    return 0;
+}
