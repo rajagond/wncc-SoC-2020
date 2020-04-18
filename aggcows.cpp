17773ee43fd1623f2210f50aa2e78825f9b31189
rajagond12 <58048685+rajagond12@users.noreply.github.com>
2020-04-18 23:40:45 +0530

    Update aggcows.cpp

--- aggcows.cpp
+++ aggcows.cpp
@@ -3,49 +3,40 @@ https://www.spoj.com/problems/AGGRCOW/
 */
 #include <bits/stdc++.h> 
 using namespace std; 
-int n,c;
+//take help from https://youtu.be/TC6snf6KPdE
    
-bool f(int val,int a[]){
-    int cows=1,pre=a[0];
+bool f(int a[],int val, int n, int c){// function to check min value possible or not
+    int c_check=1,pre=a[0];// put 1 cow at position a[0]
     for (int i=1; i<n; i++)
     {
-        if (a[i]-pre>=val)
+        if (a[i]-pre>=val)//if distance between a[i]-pre is greater than val
         {
-            pre=a[i];
-            ++cows;
-            if (cows==c)
+            pre=a[i]; // previous position of index change to a[i]
+            ++c_check; //put another cow at index a[i]
+            if (c_check==c) //if it is equal to total cow or not
                 return true;
         }
     }
     return false;
 }
 
-int binarysearch(int a[], int start,int end){
-    if(start+1>end) return start;
-    else{
-        int mid=(start+end)/2;
-        if(f(mid,a)){
-           return binarysearch(a, mid+1,end);
-        }
-        else return binarysearch(a, start,mid-1);
-    }
-}
 void solve(){
-    
+    int n,c;
     cin>>n>>c;
     int a[n];
     for(int i=0;i<n;i++) cin>>a[i];
     sort(a,a+n);
-    int start=0,end=a[n-1]-a[0],ans=-1;
-    while(start<=end){
-        int mid=(start+end)/2;
+    int start=0,end=a[n-1]-a[0],ans=-1; //here end is max distance possible and start is min distance possible
+    while(start<=end){// if start is greater is end than quit
+       //Apply binary search 
+       int mid=(start+end)/2; 
         //cout<<mid<<" "<<start<<" "<<end<<'\n';
         //cout<<ans<<'\n';
-        if(f(mid,a)){
+        if(f(mid,a,n,c)){ 
            ans=mid;
-           start=mid+1;
+           start=mid+1; //check greater value than mid. Since, we have to maximise min distance
         }
-        else end=mid-1;
+        else end=mid-1; //if mid is false than no need to check at greater value
     }
     cout<<ans<<'\n';
     return ;
