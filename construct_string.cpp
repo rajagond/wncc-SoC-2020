dfa2e6251e3254ef82426ec1f14cb9fb0b51c2d5
rajagond12 <58048685+rajagond12@users.noreply.github.com>
2020-04-25 00:12:05 +0530

    Create construct_string.cpp

---
+++ construct_string.cpp
@@ -0,0 +1,34 @@
+/* Problem Statement link
+https://codeforces.com/contest/1335/problem/B
+*/
+#include <bits/stdc++.h> 
+using namespace std; 
+
+void solve(){
+    long long int n,a,b;
+    cin>>n>>a>>b;
+    char c='a';
+
+        for(int i=0;i<n;i++) {
+           char d=c+(i%b);
+           cout<<d;
+        }
+        cout<<'\n';
+    
+   return ;
+    
+}
+int main()
+{
+//    freopen("input.txt", "r", stdin);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int t;
+    cin >> t;
+    while(t --> 0)
+        solve();
+
+    return 0;
+}
