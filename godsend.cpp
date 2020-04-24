f2583e33d0ea173957a67a66bb7e378b9e300417
rajagond12 <58048685+rajagond12@users.noreply.github.com>
2020-04-25 00:16:15 +0530

    Create godsend.cpp

---
+++ godsend.cpp
@@ -0,0 +1,23 @@
+/* Problem Statement Link
+https://codeforces.com/contest/841/problem/B
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    bool t=false;
+    for(int i=0;i<n;i++){
+        if(a[i]%2!=0){
+           t=true;
+           break;
+        } 
+    }
+    if(t) cout<<"First\n";
+    else cout<<"Second\n";
+    return 0;
+}
